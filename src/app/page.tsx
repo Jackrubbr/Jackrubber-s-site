ea8b98c375777b5875809596cdfe11e6415e394b
Jackrubbr <jackrubbr@gmail.com>
2025-05-24 18:49:02 +0200

    ho messo i quadrati con il flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,36 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { AppShell, Box, Center, Container, SimpleGrid, Skeleton, Grid, Flex} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+import type { AppProps } from 'next/app';
+import { createTheme, MantineProvider } from '@mantine/core';
+import { useState } from 'react';
+
+import styles from './page.module.css'
+
+
+export default function Demo() {
+  return (    
+    <AppShell bg="black">
+      <AppShell.Main>
+        <Container p={10} w="100%">
+          <Flex direction="row" justify="center" style={{overflowWrap:'break-word'}} gap="xs">
+            <Flex direction="column" w={600} gap="xs">
+              <div style={{border:'2px solid #cc5de8', borderRadius: '5px', width:'100%',height:'120px', color: 'white', paddingRight: '50px', paddingLeft: '50px'}}>
+                <h1 style={{color:'#fab005',fontFamily:'Pixel Nes', fontSize:'20px'}}>welcome</h1>
+                <h2 style={{color:'white', fontFamily:'MS-UIGothic',fontSize:'15px'}}>ihihihaha</h2>
+              </div>
+              
+            </Flex>
+            <Flex direction="column" w={300} gap="xs">
+              <div style={{border:'2px solid #cc5de8', borderRadius: '5px', width:'100%',height:'120px', color: 'white'}}>2</div>
+              <div style={{border:'2px solid #cc5de8', borderRadius: '5px', width:'100%',height:'120px', color: 'white'}}>4</div>
+            </Flex>
+          </Flex>
+        </Container>
+      </AppShell.Main>
+    </AppShell>
   );
 }
