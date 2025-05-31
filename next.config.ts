20cc55717bf53e35b122647a2b131442c89e4d5e
Jackrubbr <166302741+Jackrubbr@users.noreply.github.com>
2025-05-31 17:35:36 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/Jackrubber-s-site",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
-
+module.exports = nextConfig;
 export default nextConfig;
